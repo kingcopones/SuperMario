a0e6ad46f4856f46af4d3f41bb08089e120ef2cf
kingpogi123 <43062730+kingpogi123@users.noreply.github.com>
2021-10-19 10:18:08 +0800

    Initial Commit

---
+++ SuperMario/SuperMarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperMarioApp.swift
+//  SuperMario
+//
+//  Created by King Copones on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SuperMarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
